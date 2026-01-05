356efd1c873c9f169981f8458140f7b596a4eed7
Eidmanal <148284376+Eidmanal@users.noreply.github.com>
2026-01-05 20:34:56 +0200

    UI changes and audio project

--- src/data.js
+++ src/data.js
@@ -4,23 +4,27 @@ const projects = [
         description: "A website to showcase my projects and skills (this very site). ",
         link: "#",
         image: "img/project1.png",
-        stack: "HTML, CSS, JavaScript",
-        percentages: "15, 15, 70"
+        stack: "HTML, CSS, JavaScript"
     },
     {
-        title: "E-commerce Platform",
-        description: "Description for project two.",
+        title: "Automata Simulator",
+        description: "A simulator for finite automata and regular expressions. <br>It allows users to create and visualize finite automata, convert regular expressions to automata, and test strings against the automata.<br><br><b>Note: </b> This project is still in progress.",
         link: "#",
-        image: "img/project2.png",
-        stack: "HTML, CSS, JavaScript",
-        percentages: "10, 10, 80"
+        image: "img/automata.png",
+        stack: "Java, JavaFX"
     },
     {
         title: "Online Multiplayer Sudoku",
         description: "A simple sudoku game that can be played online with friends. One player hosts it as a server and the rest join as clients.<br>It features real-time updates and a scoring system based on completion time and accuracy.<br>It is built using Node.js for the server-side logic and Socket.io for real-time communication between players. <br><br><b>Note: </b> It works for both mobile and desktop browsers.",
         link: "https://github.com/Eidmanal/Sudoku",
         image: "img/sudoku.png",
-        stack: "HTML, CSS, JavaScript",
-        percentages: "20, 20, 60"
+        stack: "HTML, CSS, JavaScript"
+    },
+    {
+        title: "Audio Editor",
+        description: "A basic audio editor that allows users to cut, copy, paste, and apply simple effects to audio files. <br>It supports multiple audio formats and provides a user-friendly interface for editing.<br><br><b>Note: </b> This project is still in progress.",
+        link: "#",
+        image: "img/audio.png",
+        stack: "Java, JavaFX"
     }
 ];
